afb3a73d8b2a4e7060337ed38d2d05da718f13d8
ademaxweb <lopas1902@gmail.com>
2025-06-01 14:15:16 +0300

    Service pkg done

---
+++ pkg/srv/srv.go
@@ -0,0 +1,60 @@
+package srv
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"net/http"
+	"time"
+)
+
+type Config struct {
+	Port    int
+	Handler *mux.Router
+	Timeout time.Duration
+	Writer  io.Writer
+}
+
+type Service struct {
+	writer  io.Writer
+	port    int
+	Server  *http.Server
+	Handler http.Handler
+}
+
+func New(cfg Config) *Service {
+
+	port := 80
+	handler := mux.NewRouter()
+
+	if cfg.Port != 0 {
+		port = cfg.Port
+	}
+
+	if cfg.Handler != nil {
+		handler = cfg.Handler
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      handler,
+		WriteTimeout: cfg.Timeout,
+		ReadTimeout:  cfg.Timeout,
+		IdleTimeout:  cfg.Timeout,
+	}
+
+	return &Service{
+		Server:  srv,
+		Handler: handler,
+		writer:  cfg.Writer,
+		port:    port,
+	}
+}
+
+func (s *Service) Start() error {
+	if s.writer != nil {
+		s.writer.Write([]byte(fmt.Sprintf("Service is starting on port %d\n", s.port)))
+	}
+
+	return s.Server.ListenAndServe()
+}
