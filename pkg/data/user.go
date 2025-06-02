84368a766fce3a3d0617cef6707174320686676f
ademaxweb <lopas1902@gmail.com>
2025-06-02 15:36:27 +0300

    User data-type added

---
+++ pkg/data/user.go
@@ -0,0 +1,10 @@
+package data
+
+type User struct {
+	ID         int64  `json:"id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	CreatedAt  int64  `json:"created_at"`
+	ModifiedAt int64  `json:"modified_at"`
+}
